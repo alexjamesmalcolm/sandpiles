5220a2e6ae80bd8d758ece35b2294a6c06943df8
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-14 22:26:25 -0400

    Created BoardTest and tested shouldHaveWidthOfTenTiles

---
+++ src/main/java/com/alexjamesmalcolm/sandpiles/Board.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.sandpiles;
+
+public class Board {
+
+	public Board(int width) {
+	}
+
+	public int getWidth() {
+		return 10;
+	}
+
+}
---
+++ src/test/java/com/alexjamesmalcolm/sandpiles/BoardTest.java
@@ -0,0 +1,17 @@
+package com.alexjamesmalcolm.sandpiles;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class BoardTest {
+	
+	@Test
+	public void shouldHaveWidthOfTenTiles() {
+		int width = 10;
+		Board underTest = new Board(width);
+		int actualWidth = underTest.getWidth();
+		assertThat(actualWidth, is(width));
+	}
+}
