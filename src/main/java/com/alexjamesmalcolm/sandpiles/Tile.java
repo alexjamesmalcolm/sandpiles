49a2d9c02c13169afd3cff5d355fba722ef1c589
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-14 22:08:16 -0400

    shouldHaveSandAmountTwo

--- src/main/java/com/alexjamesmalcolm/sandpiles/Tile.java
+++ src/main/java/com/alexjamesmalcolm/sandpiles/Tile.java
@@ -2,11 +2,14 @@ package com.alexjamesmalcolm.sandpiles;
 
 public class Tile {
 
+	private int sand;
+
 	public Tile(int sand) {
+		this.sand = sand;
 	}
 
 	public int getSand() {
-		return 1;
+		return sand;
 	}
 
 }
--- src/test/java/com/alexjamesmalcolm/sandpiles/TileTest.java
+++ src/test/java/com/alexjamesmalcolm/sandpiles/TileTest.java
@@ -3,16 +3,21 @@ package com.alexjamesmalcolm.sandpiles;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 
+
 public class TileTest {
-	
+
 	@Test
 	public void shouldHaveSandAmountOne() {
 		int sand = 1;
 		Tile underTest = new Tile(sand);
 		assertThat(underTest.getSand(), is(sand));
+	}
+	
+	@Test
+	public void shouldHaveSandAmountTwo() {
+		Tile underTest = new Tile(2);
+		assertThat(underTest.getSand(), is(2));
 	}
 }
