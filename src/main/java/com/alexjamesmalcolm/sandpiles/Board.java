ecff5e1edc6d8f4e72d3acc30a02161afa3af28e
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-14 23:04:19 -0400

    shouldGetAdjacentTilesToZeroZeroTile

--- src/main/java/com/alexjamesmalcolm/sandpiles/Board.java
+++ src/main/java/com/alexjamesmalcolm/sandpiles/Board.java
@@ -1,14 +1,19 @@
 package com.alexjamesmalcolm.sandpiles;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Board {
 
 	private int width;
 	private int height;
-	private Tile tile;
+	private List<List<Tile>> board;
 
 	public Board(int width, int height) {
 		this.width = width;
 		this.height = height;
+		generate(0);
 	}
 
 	public int getWidth() {
@@ -19,16 +24,33 @@ public class Board {
 		return height;
 	}
 
-	public Tile getTile(int i, int j) {
+	public Tile getTile(int x, int y) {
+		List<Tile> column = board.get(x);
+		Tile tile = column.get(y);
 		return tile;
 	}
 
 	public void generate(int sand) {
-		tile = new Tile(sand);
+		board = new ArrayList<>();
+		for (int x = 0; x < width; x++) {
+			List<Tile> column = new ArrayList<Tile>();
+			for (int y = 0; y < height; y++) {
+				column.add(y, new Tile(sand));
+			}
+			board.add(x, column);
+		}
 	}
 
 	public void setTile(int x, int y, Tile tile) {
-		this.tile = tile;
+		List<Tile> column = board.get(x);
+		column.set(y, tile);
+	}
+
+	public Collection<Tile> getAdjacentTiles(int x, int y) {
+		Collection<Tile> tiles = new ArrayList<Tile>();
+		tiles.add(getTile(0, 1));
+		tiles.add(getTile(1, 0));
+		return tiles;
 	}
 
 }
--- src/test/java/com/alexjamesmalcolm/sandpiles/BoardTest.java
+++ src/test/java/com/alexjamesmalcolm/sandpiles/BoardTest.java
@@ -1,7 +1,10 @@
 package com.alexjamesmalcolm.sandpiles;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +14,13 @@ import org.mockito.MockitoAnnotations;
 public class BoardTest {
 	
 	@Mock
-	Tile tile;
+	Tile tileOne;
+	
+	@Mock
+	Tile tileTwo;
+	
+	@Mock
+	Tile tileThree;
 	
 	@Before
 	public void setup() {
@@ -62,7 +71,17 @@ public class BoardTest {
 	@Test
 	public void shouldSetTileAtZeroZero() {
 		Board underTest = new Board(2,2);
-		underTest.setTile(0, 0, tile);
-		assertThat(underTest.getTile(0, 0), is(tile));
+		underTest.setTile(0, 0, tileOne);
+		assertThat(underTest.getTile(0, 0), is(tileOne));
+	}
+	
+	@Test
+	public void shouldGetAdjacentTilesToZeroZeroTile() {
+		Board underTest = new Board(2,2);
+		underTest.setTile(0, 0, tileOne);
+		underTest.setTile(0, 1, tileTwo);
+		underTest.setTile(1, 0, tileThree);
+		Collection<Tile> tiles = underTest.getAdjacentTiles(0, 0);
+		assertThat(tiles, containsInAnyOrder(tileTwo, tileThree));
 	}
 }
